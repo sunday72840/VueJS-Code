a4097c7ef50da2b758f33d3c479f02a781ece253
sunday72840 <c58580@163.com>
2021-09-16 11:38:21 +0800

    2021-09-16

--- LearnVuejs08/learnvuex/src/App.vue
+++ LearnVuejs08/learnvuex/src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app">
+    
+    <h2>------------app内容:modules中的内容</h2>
+    <h2>{{$store.state.a.name}}</h2>
+    <button @click="updateName">修改名字</button>
+    <h2>{{$store.getters.fullName}}</h2>
+    <h2>{{$store.getters.fullName2}}</h2>
+    <h2>{{$store.getters.fullName3}}</h2>
+    <button @click="asyncUpdateNme">异步修改</button>
+
+    <h2>------------app内容:info对象的内容是否是响应式的</h2>
+    <h2>{{$store.state.info}}</h2>
+    <button @click="updateInfo">修改信息</button>
     <h2>=-=-=-=-=-=-=-=-=-=APP内容=-=-=-=-=-=-=-=-=-=-=-=</h2>
     <h2>{{$store.state.counter}}</h2>
     <!-- <h2>{{counter}}</h2> -->
@@ -15,6 +27,8 @@
     <!-- <h1>{{$store.getters.powerCounter}}</h1>
     <h2>{{$store.getters.more20stu}}</h2>
     <h2>{{$store.getters.more20stuLength}}</h2> -->
+
+
     <h2>{{$store.getters.moreAgestu(20)}}</h2>
     <h2>-=-=-=-=-=-=-=-=-=-=-=-=-Hello VueX的内容-=-=-=-=-=-=-=-=-=-=- </h2>
     <hello-vue-x></hello-vue-x>
@@ -24,6 +38,8 @@
 <script>
 
 import HelloVueX from './components/HelloVueX.vue'
+// 导入抽离出去的常量js文件
+import {INCREMENT} from './store/mutations-types'
 export default {
   name: 'App',
   components:{
@@ -41,7 +57,7 @@ export default {
   // },
   methods:{
     addition (){
-      this.$store.commit('increment')
+      this.$store.commit(INCREMENT)
     },
     subtraction(){      
       this.$store.commit('decrement')
@@ -59,6 +75,28 @@ export default {
     addStudent (){
       const stu = {id:116,name:'alan',age:35}
       this.$store.commit('addStudent',stu)
+    },
+    updateInfo (){
+      // this.$store.commit('updateInfo')
+      // 如果回调多个参数可以在后面使用一个对象{}
+      // this.$store.dispatch('aUpdateInfo',{
+      //   message:'我是携带的信息',
+      //   success :() =>{
+      //     console.log('里面回调已完成！');
+      //   }
+      // })
+      // 这里传入两个或者多个参数供index.js里的aUpdateInfo构造函数调用
+      // 这里的then是index.js文件中Promise里的一个then方法
+      this.$store.dispatch('aUpdateInfo','我是携带的信息').then(res =>{
+        console.log('里面完成了提交!');
+        console.log(res);
+      })
+    },
+    updateName(){
+      this.$store.commit('updateName','lisi')
+    },
+    asyncUpdateNme(){
+      this.$store.dispatch('aUpdateName')
     }
   }
 }
--- LearnVuejs08/learnvuex/src/store/index.js
+++ LearnVuejs08/learnvuex/src/store/index.js
@@ -1,10 +1,46 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// 导入常量文件
+import { INCREMENT } from './mutations-types'
 
 // 1.安装插件
 Vue.use(Vuex)
 
 // 2.创建对象
+const moduleA = {
+    state: {
+        name: 'zhangsan'
+    },
+    mutations: {
+        updateName(state, payload) {
+            state.name = payload
+        }
+    },
+    actions: {
+        // context:上下文 
+        // VueX里更改状态进行异步操作(网络请求)必须在actions里进行异步代码编写
+        aUpdateName(context) {
+            // 这里的commit是属于moduleA的
+            setTimeout(() => {
+                context.commit('updateName', 'wangwu')
+            }, 1000)
+        }
+    },
+    getters: {
+        // fullName和fullName2是层级关系，fullName2可以直接把整个getter对象传入
+        fullName(state) {
+            return state.name + '1111'
+        },
+        fullName2(state, getters) {
+            return getters.fullName + '2222'
+        },
+        // 如果看不明白可以先把程序运行一下
+        // 第三个fullName3 可以传3个参数，这里的rootState是调用下面定义的store里的counter
+        fullName3(state, getters, rootState) {
+            return getters.fullName2 + rootState.counter
+        }
+    }
+}
 const store = new Vuex.Store({
     state: {
         counter: 1000,
@@ -14,10 +50,16 @@ const store = new Vuex.Store({
             { id: 112, name: 'james', age: 34 },
             { id: 113, name: 'curry', age: 10 },
             { id: 114, name: 'sss', age: 18 },
-        ]
+        ],
+        info: {
+            name: 'kobe',
+            age: 40,
+            height: 1.98
+        }
     },
     mutations: {
-        increment(state) {
+        // 另一种定义方法的写法
+        [INCREMENT](state) {
             state.counter++
         },
         decrement(state) {
@@ -28,10 +70,43 @@ const store = new Vuex.Store({
         },
         addStudent(state, stu) {
             state.students.push(stu)
+        },
+        updateInfo(state) {
+            state.info.name = 'codewhy'
+                // // 不能在这里进行异步操作
+                // setTimeout(() => {
+                //     state.info.name = 'codewhy'
+                // }, 1000);
+                // state.info['addres'] = '洛杉矶'
+                // 通过vue.set的方法把添加的对象变成响应式
+                // Vue.set(state.info, 'address', '洛杉矶')
+                // Vue.delete(state.info, 'age')
         }
     },
+    // context:上下文 
+    // VueX里更改状态进行异步操作(网络请求)必须在actions里进行异步代码编写
     actions: {
+        // 第一种写法
+        // aUpdateInfo(context, payload) {
+        //     setTimeout(() => {
+        //         // 先提交commit,然后再回调payload()
+        //         context.commit('updateInfo')
+        //         console.log(payload.message); //可以用payload回调APP组件中定义的message方法
+        //         payload.success() //也可以用payload回调参数来调用app组件中的success构造函数
+        //     }, 1000);
+        // },
 
+        // 第二种写法
+        aUpdateInfo(context, payload) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    context.commit('updateInfo')
+                    console.log(payload);
+                    // resolve回调完的数据返回到app组件中then里的res打印结果
+                    resolve('11111111')
+                }, 1000)
+            })
+        }
     },
     getters: {
         powerCounter(state) {
@@ -52,8 +127,9 @@ const store = new Vuex.Store({
             }
         }
     },
+    // 可以把上面的几个方法抽离成一个模块化管理
     modules: {
-
+        a: moduleA
     }
 })
 
